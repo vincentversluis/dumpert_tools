0f79d25a569996333ccfb97a04740768adc30464
vincentversluis <vincentversluis@hotmail.com>
2025-01-23 18:48:56 +0100

    Improved comments and README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dumpert_tools
-Some tools for dumpert scraping and viewing
+# Dumpert tools
+
+Stuff that helps navigate the Dutch video sharing site [dumpert.nl](https://www.dumpert.nl/). At the moment this only contains one script and it is very unlikely more things will be added.
+
+## [Worst video](worst_video.py)
+
+This script finds the worst rated videos in a given month on [dumpert.nl](https://www.dumpert.nl/). Users can plus or minus videos. Usually ragebaitvideos are rated negatively, which is exactly what this script is intended to find.
--- worst_video.py
+++ worst_video.py
@@ -1,3 +1,18 @@
+# %% HEADER
+# This script is written as an interactive notebook that requires VS Code with the
+# Jupyter extension installed. The # %% delimiters are used to mark code cells, 
+# mimicking the way Jupyter notebooks work.
+#
+# This script finds the worst rated videos in a given month on the Dutch video sharing 
+# site dumpert.nl. On this site users can plus or minus videos. Usually ragebait
+# videos are rated negatively, which is exactly what I intend to find. 
+# 
+# As the script relies on the browser to scroll down the page, it is best used to
+# find the worst videos of the current or previous month. Very limited manual help
+# might be required, though this is limited to manually scrolling down in a browser
+# window, if the programmatic scrolling turns out to be too slow. Pay attention 
+# to the comments within the script to signal these manual supports.
+
 # %% IMPORTS
 import arrow
 from tqdm import tqdm
@@ -8,18 +23,19 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException                         
 
 # %% CONFIG
-month_of_interest = 'december' # Full Dutch month name
-year_of_interest = '2024' # YYYY
-bottom_n = 25
+month_of_interest = 'januari'  # Full Dutch month name
+year_of_interest = '2025'  # YYYY
+bottom_n = 25  # Number of videos to output
 
 # %% CONSTANTS
+# Chrome cookies are usually stored in this folder:
 PATH_COOKIES = "C:\\Users\\vince\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Network\\"
 LINKS = {
     'dumpert':'https://www.dumpert.nl/',
     'dumpert_toppers_maand': 'https://www.dumpert.nl/toppers/maand',
     }
 MONTHS_NL = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december']
-LAST_OF_MONTH = {
+LAST_OF_MONTH = {  # Could not be bothered to faff with functions for this
     'januari': 31,
     'februari': 28,
     'maart': 31,
@@ -39,7 +55,15 @@ def get_prev_daymonthyear_of_interest(
         month_of_interest: str,
         year_of_interest: str
         ) -> str:
-    """Return string version of previous month/year of interest"""
+    """Return string version of previous month/year of interest, as used on dumpert.nl
+
+    Args:
+        month_of_interest (str): Target month
+        year_of_interest (str): Target year
+
+    Returns:
+        str: Formatted string of month/year as used on dumpert.nl
+    """
     month_of_interest = month_of_interest.lower()
     prev_month_of_interest = MONTHS_NL[MONTHS_NL.index(month_of_interest)-1]
     prev_year_of_interest = year_of_interest if month_of_interest != MONTHS_NL[-1] else str(int(year_of_interest)-1)
@@ -51,13 +75,20 @@ def clean_dumpert_datestring(datestring: str) -> str:
         30 apr. '24 @ 22:04 | 40.634 views
     to
         30 apr '24    
+        
+    Args:
+        datestring (str): Datestring as used on dumpert.nl
+
+    Returns:
+        str: Formatted string of month/year as recognisable by arrow
     """
     return datestring.split('@')[0].replace('.', '').strip()
 
 # %% MAIN
+# Open the browser and go to the dumpert.nl home page
+# !! It helps having the browser window maximised
 print("Opening a Browser")
 chrome_options = webdriver.ChromeOptions() 
-# Add regular cookies
 chrome_options.add_argument(f"user-data-dir={PATH_COOKIES}")
 driver = webdriver.Chrome(options=chrome_options)
 address = LINKS['dumpert']
@@ -72,14 +103,13 @@ prev_dmy_of_interest = get_prev_daymonthyear_of_interest(
 print(f"Scrolling down page to videos dated {prev_dmy_of_interest}")
 print("Manual interruption at that date is fine.")
 # Scroll down a good many times -- every month many videos are uploaded
-# !! It helps having the browser window maximised
 main_page = driver.find_element(By.TAG_NAME, 'html')
 for _ in range(600):
     main_page.send_keys(Keys.PAGE_DOWN)
 
 print("Scrolled a good way down, manually scroll further if necessary")
 # %%
-# Try clicking 'open meer items'
+# !! Click 'open meer items' if the first day of the previous month is not reached 
 
 # %% Get videos and details
 videos = {
@@ -90,20 +120,19 @@ videos = {
 
 # Get video details
 for video_id, video in tqdm(videos.items(), desc="Retrieving video details"):
-    # Title
     title = video['css'].find_element(By.CLASS_NAME, "css-hp3yqo").text
-    # url
     url = video['css'].get_attribute('href')
-    # Date
     datestring = video['css'].find_element(By.CLASS_NAME, "css-13w4nb7").text
     datestring = clean_dumpert_datestring(datestring)
+    
     if 'vandaag' in datestring:
         date = arrow.utcnow().to('CET').date()
     elif 'gister' in datestring:
         date = arrow.utcnow().to('CET').shift(days=-1).date()
     else:
         date = arrow.get(datestring, "D MMM 'YY", locale='nl-NL').date()
-    # Score
+        
+    # Get score
     try:
         score = int(video['css'].find_element(By.CLASS_NAME, "css-1taeduy").text)
     except NoSuchElementException: # Negative score has a different class
@@ -118,7 +147,8 @@ for video_id, video in tqdm(videos.items(), desc="Retrieving video details"):
         'score':      score
         })
 
-# %%
+print(f"Found {len(videos)} videos")
+
 # Adjust year of interest if month is last month of year
 adjusted_year_of_interest = year_of_interest if month_of_interest != MONTHS_NL[-1] else str(int(year_of_interest)-1)
 
@@ -134,25 +164,25 @@ end_period_of_interest = arrow.get(
     , locale='nl-NL'
     ).date()
 
-# %%
 # Filter videos for date
 videos = {k: v for k, v in videos.items() if start_period_of_interest <= v['date'] <= end_period_of_interest}
 
-# %%
 # Sort videos by score
 videos = dict(sorted(videos.items(), key=lambda x:x[1]['score']))
 
-# %%
 # Get shit list and show outcome
 videos_shit = dict(list(videos.items())[:bottom_n])
 for k, v in videos_shit.items():
     print(f"{v['score']:<5} - {v['title']}")
 
-# %% Shut down browser when done collecting and sorting shit list
+# %% 
+# Shut down browser when done collecting and sorting shit list. This is a separate
+# step, as for debugging purposes, which are hopefully not needed, this saves having
+# to open the browser and scroll all the way down again.
 driver.close()
 
 # %%
-# Open shit list
+# Open each video of the shit list in a new tab
 print("Opening a Browser")
 chrome_options = webdriver.ChromeOptions() 
 # Add regular cookies
@@ -164,11 +194,8 @@ for k, v in videos_shit.items():
     driver.execute_script("window.open('');") 
     # Switch to the new window and open new URL 
     driver.switch_to.window(driver.window_handles[-1]) 
-    # driver.get(v['url'])
     
 # %%
-driver.close()
-
-# %%
-videos_shit
-# %%
+# Close the browser, though user probably wants to do this themselves after enjoying
+# the ragebait on the shit list
+# driver.close()
